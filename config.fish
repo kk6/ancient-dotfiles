adf1fb44d06e8632099950f7d6ff05b0795cc0c8
kk6 <hiro.ashiya@gmail.com>
2017-07-14 15:50:34 +0900

    :heavy_plus_sign: Add config.fish

---
+++ config.fish
@@ -0,0 +1,11 @@
+# alias
+alias l "ls"
+alias mkdirs "mkdir -p"
+alias em "emoji/.venv/bin/em"
+
+set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
+
+# autoenv
+source /usr/local/opt/autoenv_fish/activate.fish
+
