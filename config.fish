13fbe20e77d122bab339f580abd2a2688429c25a
kk6 <hiro.ashiya@gmail.com>
2017-07-26 10:33:25 +0900

    :pencil2: Write direnv settings in config.fish

--- config.fish
+++ config.fish
@@ -6,6 +6,5 @@ alias em "emoji/.venv/bin/em"
 set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
 
-# autoenv
-source /usr/local/opt/autoenv_fish/activate.fish
-
+# direnv
+eval (direnv hook fish)
