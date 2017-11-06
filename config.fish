7d54c86459cd6d0ce351ec69ea90bcc7cdc544ab
kk6 <hiro.ashiya@gmail.com>
2017-11-06 17:54:36 +0900

    :pencil2: Update config.fish for pipenv

--- config.fish
+++ config.fish
@@ -22,3 +22,8 @@ set fish_custom $HOME/.config/fish
 
 # load secret config (API keys, etc.)
 . $fish_custom/config.secret.fish
+
+# for pipenv
+set -g -x PIPENV_DEFAULT_PYTHON_VERSION 3
+set -g -x PIPENV_SHELL_FANCY 1
+
