327ddd3a445d404a804f6328f1dbd6e4c006621f
kk6 <hiro.ashiya@gmail.com>
2017-11-22 11:22:47 +0900

    :pencil2: Update config.fish some aliases.

--- config.fish
+++ config.fish
@@ -1,7 +1,10 @@
 # alias
 alias l "ls"
 alias mkdirs "mkdir -p"
-alias em "emoji/.venv/bin/em"
+alias pe "pipenv"
+alias per "pipenv run"
+alias pes "pipenv shell"
+
 
 set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
@@ -10,11 +13,11 @@ set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
 eval (direnv hook fish)
 
 # for npm / yarn
-set -g -x PATH $HOME/.npm-global/bin $PATH
+set -x PATH $HOME/.npm-global/bin $PATH
 
-set -g -x PATH /usr/local/bin $PATH
-set -g -x PYTHONUSERBASE $HOME/.local
-set -g -x PATH $PYTHONUSERBASE/bin $PATH
+set -x PATH /usr/local/bin $PATH
+set -x PYTHONUSERBASE $HOME/.local
+set -x PATH $PYTHONUSERBASE/bin $PATH
 
 
 # Path to your custom folder (default path is $FISH/custom)
@@ -24,7 +27,7 @@ set fish_custom $HOME/.config/fish
 . $fish_custom/config.secret.fish
 
 # for pipenv
-set -g -x PIPENV_DEFAULT_PYTHON_VERSION 3
-set -g -x PIPENV_SHELL_FANCY 1
-set -g -x PIPENV_VENV_IN_PROJECT 1
+set -x PIPENV_DEFAULT_PYTHON_VERSION 3
+set -x PIPENV_SHELL_FANCY 1
+set -x PIPENV_VENV_IN_PROJECT 1
 
